8d9f4594be318130a4901780a2e0422999ff3cd7
faisalahmed11 <faisalbaloch09876@gmail.com>
2024-10-07 05:47:23 -0700

    completed 16/16 chapters from nextjs.org learn and fixed linting error

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <div>Customers page</div>;
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Customers",
+};
+export default async function Page() {
+  return <div>Customers pages</div>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,17 @@ import "@/app/ui/global.css";
 import { Analytics } from "@vercel/analytics/react";
 
 import { inter } from "./ui/fonts";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Al Mehran Metal",
+    default: "Al Mehran Metal",
+  },
+  description:
+    "We deal with all type of metal scraping and reselling services in UAE, Dubai, Abu Dhabi, Sharja, Satwa",
+  metadataBase: new URL("https://almehranmetal.com/dashboard"),
+};
 export default function RootLayout({
   children,
 }: {
